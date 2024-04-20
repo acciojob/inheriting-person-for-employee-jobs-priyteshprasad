1f708a11301991cde1c2fee8f4e4350965de3c89
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-04-20 12:43:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,27 @@
-// complete this js code
-let personPropObj = {
+class Person{
+	constructor(name, age){
+		this.name = name;
+		this.age = age;
+	}
 	greet(){
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 	}
 }
-let employeePropObj = {
+class Employee extends Person{
+	constructor(name, age, jobTitle){
+		super(name, age)
+		this.jobTitle = jobTitle
+	}
 	jobGreet(){
+
 		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
 	}
 }
-Object.setPrototypeOf(employeePropObj, personPropObj)
-
-function Person(name, age) {
-	let p = Object.create(personPropObj)
-	p.name = name;
-	p.age = age;
-	return p;
-}
-
-function Employee(name, age, jobTitle) {
-	let emp = Person(name, age);
-	emp.jobTitle = jobTitle;
-	Object.setPrototypeOf(emp, employeePropObj)
-	
-	return emp; 
-}
-// let p1 = Person("Alice", 25)
+// let p1 = new Person("Alice", 25)
 // p1.greet()
-// let e1 = Employee("Bob", 30, "Manager")
+// let e1 = new Employee("Bob", 30, "Manager")
 // e1.jobGreet() 
+// console.log(e1)
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
