b469e05176523bc4608c7d834e01aa9a98b06f80
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-04-20 12:00:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	let p = {}
+	p.name = name;
+	p.age = age;
+	p.greet = function(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
+	return p;
+}
 
-function Employee(name, age, jobTitle) {}
-
+function Employee(name, age, jobTitle) {
+	let emp = Person(name, age);
+	emp.jobTitle = jobTitle;
+	emp.jobGreet = function(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`)
+	}
+	return emp; 
+}
+// let p1 = Person("Alice", 25)
+// p1.greet()
+// let e1 = Employee("Bob", 30, "Manager")
+// e1.jobGreet() 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
